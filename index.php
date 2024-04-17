0c723f691f63db334cc52da164cd1516cc9b9bb9
Kim Ji Yu <spbabo97@naver.com>
2024-04-18 04:23:58 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+    include_once $_SERVER["DOCUMENT_ROOT"]."/layout/_head.php";
+    include_once $_SERVER["DOCUMENT_ROOT"]."/components/header.html";
+?>
+
+<main class="_main">
+
+    <?php
+        include $_SERVER["DOCUMENT_ROOT"]."/components/main/section01.html";
+    ?>
+
+    <section class="sec02">
+        <h3 class="h3">
+            <p>더 빠르고 부드러운 시력 교정술,</p>
+            <p><span>자이스 스마일라식</span></p>
+        </h3>
+        <p class="tbx">
+            아름다운 눈, 소중한 눈, 눈의 건강을 지키기 위해 끊임없이 노력하는 예일안과는<br/>
+            안전한 시력 교정을 위해 스마일 수술을 개발한 자이스 장비를 도입했습니다. <br/>
+            본원은 회복과 시력의 질 향상을 위해 숙련된 의료진이 검사부터, 수술, 사후 관리까지 <br/>
+            <span>1:1 맞춤형으로 커스터마이징 스마일 라식을 추구</span>합니다. 
+        </p>
+    </section>
+
+    <section class="sec02"></section>
+
+    <section class="sec03"></section>
+
+    <section class="sec04"></section>
+
+    <section class="sec05"></section>
+
+    <section class="sec06"></section>
+
+    <?php
+        include_once $_SERVER["DOCUMENT_ROOT"]."/components/footer.html";
+    ?>
+
+</main>
+
+<?php
+    include_once $_SERVER["DOCUMENT_ROOT"]."/layout/_end.php";
+?>
---
+++ layout/_head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- 반응형 및 ios-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/> 
+        <meta name="format-detection" content="telephone=no, address=no, email=no" />
+        <title>타이틀</title>
+        <!-- CSS -->
+        <link rel="stylesheet" as="style" crossorigin href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/reset.css">
+        <link rel="stylesheet" href="/css/scroll.css">
+        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/scss/kjy/main.css">
+
+        <!-- FONT -->
+        <link rel="stylesheet" href="/font/font.css">
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
+
+        <!-- SCRIPT -->
+
+    </head>
+<body>
